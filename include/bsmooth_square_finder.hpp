30ec949426a3f983e7740bf1a6fd16a36fd0d3cf
Jonathan Lee <bukatea@gmail.com>
2022-04-03 15:11:28 -0400

    Implement full bsmoothsquarefinder

--- include/bsmooth_square_finder.hpp
+++ include/bsmooth_square_finder.hpp
@@ -7,11 +7,20 @@
 #include "common.hpp"
 #include "tonelli_shanks.hpp"
 
+struct BSmoothSquare {
+	bmp::mpz_int x;
+	// size is piB
+	std::vector<std::size_t> primeFactors;
+
+	BSmoothSquare(const bmp::mpz_int &x, const std::vector<std::size_t> &primeFactors);
+};
+
+// add support for multiple finds
 class BSmoothSquareFinder {
 	public:
 		BSmoothSquareFinder(const bmp::mpz_int &n, const std::vector<std::size_t> &factorBase, std::size_t A);
 
-		std::vector<bmp::mpz_int> find();
+		std::vector<BSmoothSquare> find(std::size_t numBSmoothSquares);
 
 	private:
 		static const bmp::mpf_float ERROR = "0.000000000000000000000000000000000000000000000000005"
@@ -23,6 +32,8 @@ class BSmoothSquareFinder {
 		std::size_t A;
 
 		std::vector<bmp::mpf_float_50> sieve;
+		std::vector<std::size_t> primeExponentsLeqA;
+		std::vector<std::vector<std::size_t>> primeFactors;
 };
 
 #endif
--- src/bsmooth_square_finder.cpp
+++ src/bsmooth_square_finder.cpp
@@ -1,44 +1,72 @@
 #include "bsmooth_square_finder.hpp"
 
+BSmoothSquare::BSmoothSquare(const bmp::mpz_int &x, const std::vector<std::size_t> &primeFactors) : x(x), primeFactors(primeFactors) {}
+
 BSmoothSquareFinder::BSmoothSquareFinder(const bmp::mpz_int &n, const std::vector<std::size_t> &factorBase, std::size_t A)
-	: n(n), s(static_cast<bmp::mpz_int>(std::sqrt(n)) + 1), factorBase(factorBase), piB(factorBase.size()), A(A), sieve(A + 1) {
+	: n(n),
+	  s(static_cast<bmp::mpz_int>(std::sqrt(n)) + 1),
+	  factorBase(factorBase),
+	  piB(factorBase.size()),
+	  A(A),
+	  sieve(A + 1),
+	  primeExponentsLeqA(piB),
+	  primeFactors(A + 1, std::vector<std::size_t>(piB)) {
 	// initialize sieve with ln f(x)
 	// sieve will look like [0, 1, ..., A]
+	// also initialize primeExponentsLeqA, primeExponentsLeqA[i] is the largest n s.t. factorBase[i]^n <= A
 	// add negatives?
 	for (std::size_t x = 0; x < A + 1; x++) {
 		bmp::mpz_int base = s + x;
 		sieve[x] = std::log(static_cast<bmp::mpf_float_50>(base * base - n));
 	}
+
+	for (std::size_t i = 0; i < piB; i++) {
+		primeExponentsLeqA[i] = static_cast<std::size_t>(std::log(A) / std::log(factorBase[i]));
+	}
 }
 
-std::vector<bmp::mpz_int> BSmoothSquareFinder::find() {
-	std::vector<bmp::mpz_int> res;
-	for (std::size_t p : factorBase) {
-		QuadraticResidueSquareRoot res = tonelliShanks(n, p);
-		if (!res.exists) {
-			continue;
-		}
+std::vector<BSmoothSquare> BSmoothSquareFinder::find(std::size_t numBSmoothSquares) {
+	const static bmp::mpf_float SIEVE_ERROR = piB * ERROR;
 
-		bmp::mpf_float_50 logp = std::log(static_cast<bmp::mpf_float_50>(p));
-
-		bmp::mpz_int x = res.root1 - s;
-		while (x < A + 1) {
-			sieve[x] -= logp;
-			if (-piB * ERROR <= sieve[x] && sieve[x] <= piB * ERROR) {
-				// sieve[x] is approx. 0, B-smooth number found
-				res.push_back(x + s);
+	std::vector<BSmoothSquare> res;
+	for (std::size_t i = 0; i < piB; i++) {
+		for (std::size_t k = primeExponentsLeqA[i]; k >= 0; k--) {
+			std::size_t divisor = std::pow(factorBase[i], k);
+			QuadraticResidueSquareRoot res = tonelliShanks(n, divisor);
+			if (!res.exists) {
+				continue;
 			}
-			x += p;
-		}
-		if (p != 2) {
-			x = res.root2 - s;
+
+			bmp::mpf_float_50 logd = std::log(static_cast<bmp::mpf_float_50>(divisor));
+
+			bmp::mpz_int x = res.root1 - s;
 			while (x < A + 1) {
-				sieve[x] -= logp;
-				if (-piB * ERROR <= sieve[x] && sieve[x] <= piB * ERROR) {
+				sieve[x] -= logd;
+				primeFactors[x][i]++;
+				if (-SIEVE_ERROR <= sieve[x] && sieve[x] <= SIEVE_ERROR) {
 					// sieve[x] is approx. 0, B-smooth number found
-					res.push_back(x + s);
+					res.emplace_back(x + s, primeFactors[x]);
+					if (res.size() == numBSmoothSquares) {
+						return res;
+					}
 				}
-				x += p;
+				x += divisor;
+			}
+			if (factorBase[i] != 2) {
+				// if the prime is 2 the roots should be the same, 1 (mod 2)
+				x = res.root2 - s;
+				while (x < A + 1) {
+					sieve[x] -= logd;
+					primeFactors[x][i]++;
+					if (-SIEVE_ERROR <= sieve[x] && sieve[x] <= SIEVE_ERROR) {
+						// sieve[x] is approx. 0, B-smooth number found
+						res.emplace_back(x + s, primeFactors[x]);
+						if (res.size() == numBSmoothSquares) {
+							return res;
+						}
+					}
+					x += divisor;
+				}
 			}
 		}
 	}
